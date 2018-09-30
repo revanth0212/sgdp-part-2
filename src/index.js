f107bdad0972d1d92d32b527ecdbca37afcffd35
Revanth <revanth0212@gmail.com>
2018-09-30 11:12:12 -0500

    Added redux.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Fabric } from 'office-ui-fabric-react/lib/Fabric'
+import { Provider } from 'react-redux'
 
 import AppView from './App'
+import Store from './App/Store'
 
 import './styles.css'
 
@@ -10,7 +12,9 @@ function App() {
   return (
     <div className="App">
       <Fabric>
-        <AppView />
+        <Provider store={Store}>
+          <AppView />
+        </Provider>
       </Fabric>
     </div>
   )
